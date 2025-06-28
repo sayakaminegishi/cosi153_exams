eac07c40f7bd10fc586cfc37868e5d9ab294c51c
“Sayaka <saya1113saya@gmail.com>
2025-06-28 17:45:07 -0400

    final

--- Exam4Pt1/server.js
+++ Exam4Pt1/server.js
@@ -1,17 +1,24 @@
-const express = require('express');
-const path = require('path');
+//exam 4 pt 1
 
-const app = express();
-const port = 3000;
+//import necessary modules
+const express = require('express'); //web framework for Node.js
+const path = require('path'); //to handle file paths 
+const localIP = '192.168.0.22'; //IP address of computer
 
-// Serve static CSS file
+const app = express(); //initialize an instance of an Express application to handle routing and HTTP requests.
+const port = 3000; //the server will listen on port 3000
+
+// Serve static files from the Public directory
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
-// Route for the screen
+// Route for the screen: when the browser visits /screen, the router will send the screen.html file in the Public directory. 
 app.get('/screen', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'screen.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/screen`);
+
+//start server and logs a message in the console with URL to the /screen page. 
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running at localIP:${port}/screen`); //accepts request from any device on the same local network
 });
+
